8efee2ec5c6b27bdc571eca9218746f4418d00a1
Sahib-Aujla <sahibpreet568@gmail.com>
2025-08-28 09:38:29 +0530

    code refactor and fork test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,15 +3,22 @@ pragma solidity ^0.8.18;
 
 import "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundeMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() public {
-        fundMe = new FundMe();
+        DeployFundeMe d = new DeployFundeMe();
+        fundMe = d.run();
     }
 
-    function testMinUSD() public {
+    function testMinUSD() public view {
         assertEq(fundMe.MINIMUM_USD(), 5 * 10 ** 18);
+    }
+
+    function testGetVersion() public view{
+        uint p = fundMe.getVersion();
+        assertEq(p, 4);
     }
 }
