a875d4d13c2c725564486a73b8546e3e98840bf3
Sahib-Aujla <sahibpreet568@gmail.com>
2025-08-30 11:10:36 +0530

    added ffi config

---
+++ script/Interactions.s.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+contract FundFundMe is Script {}
