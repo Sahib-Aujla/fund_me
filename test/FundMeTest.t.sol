88e4e707aa1c1f30a302f83c2692f882606d127d
Sahib-Aujla <sahibpreet568@gmail.com>
2025-08-30 10:40:55 +0530

    c0mpleted test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -7,9 +7,12 @@ import {DeployFundeMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address USER = makeAddr("user");
+    DeployFundeMe d;
 
     function setUp() public {
-        DeployFundeMe d = new DeployFundeMe();
+        vm.deal(USER, 100 ether);
+        d = new DeployFundeMe();
         fundMe = d.run();
     }
 
@@ -20,5 +23,44 @@ contract FundMeTest is Test {
     function testGetVersion() public view {
         uint256 p = fundMe.getVersion();
         assertEq(p, 4);
+    }
+
+    function testFundSuccessfully() public {
+        //fund with eth
+        vm.prank(USER);
+        fundMe.fund{value: 1 ether}();
+        //check if the funding is done
+        assertEq(1 ether, fundMe.addressToAmountFunded(USER));
+    }
+
+    function testFundRevert() public {
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.fund{value: 0 ether}();
+    }
+
+    function testWithdraw() public {
+        //fund and then withdraw
+        vm.prank(USER);
+        fundMe.fund{value: 1 ether}();
+
+        uint256 startingOwnerBalance = fundMe.i_owner().balance;
+        uint256 startingContractBalance = address(fundMe).balance;
+
+        vm.prank(fundMe.i_owner()); // ✅ withdraw as owner
+        fundMe.withdraw();
+
+        uint256 endingOwnerBalance = fundMe.i_owner().balance;
+        uint256 endingContractBalance = address(fundMe).balance;
+
+        assertEq(endingContractBalance, 0);
+        assertEq(
+            startingOwnerBalance + startingContractBalance,
+            endingOwnerBalance
+        );
+    }
+
+    function testOwner() external view{
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 }
